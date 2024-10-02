6abb2d228ec00713e2be8ae7ccabea857e978ccc
Satyajit Gokhale <twisterintwist@gmail.com>
2024-10-02 11:03:21 -0400

    Automated transformation to reduce re-rendering

--- frontend/src/components/Player.js
+++ frontend/src/components/Player.js
@@ -1,24 +1,19 @@
+import { memo } from "react";
 /**
  * The player page
  * @author Oren Sokoler
  */
 
-import {getAPIurl} from './Config.js'
-
+import { getAPIurl } from './Config.js';
 import React from 'react';
 import '../css/Page.css';
 
 /**
  * Render the Mopidy-Iris player in an iframe
  */
-function Player() {
-    return (
-        <div className='Page-container'>
-          <iframe title='Mopidy-Iris' 
-                  src={getAPIurl() + '/iris/'}
-                  className='Page-content'/>
-        </div>
-    );
-}
-
+const Player = memo(function Player() {
+  return <div className='Page-container'>
+          <iframe title='Mopidy-Iris' src={getAPIurl() + '/iris/'} className='Page-content' />
+        </div>;
+});
 export default Player;
